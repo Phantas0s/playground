c750176f5ddfa50f90269e25eea5f44b55694e50
matthieu <matthieu.cneude@gmail.com>
2020-03-28 19:30:58 +0100

    Try to solve clojure problem

--- clojure/brave-clojure/7-read-eval-macro/exercises.clj
+++ clojure/brave-clojure/7-read-eval-macro/exercises.clj
@@ -4,7 +4,23 @@
 '( 2 3)
 
 ; Create an infix function that takes a list like (1 + 3 * 4 - 5) and transforms it into the lists that Clojure needs in order to correctly evaluate the expression using operator precedence rules.
+(defmacro first-priority [infixed]
+  (loop [base infixed
+         new-list ()
+         list-index 0
+         current-val (first infixed)]
+    (let [next-val (nth infixed (inc list-index))
+          prev-val (nth infixed (dec list-index))]
+      (cond  
+        (= list-index (count (- new-list 1))) (new-list)
+        (or (= current-val "*") ( current-val "/")) (recur base (conj new-list (list current-val prev-val next-val)) (inc list-index) (nth infixed (inc list-index))) 
+        :else (recur base (conj new-list current-val) (inc list-index) (nth infixed (inc list-index)))
+        ))))
 
-(defmacro infix
-  [infixed]
-  ())
+(first-priority (1 + 3 * 2 - 5))
+
+; (defmacro infix
+;   [infixed]
+
+;   (loop [])
+;   )
