6dc3cd1407ce5386fb59d7a4dc2335ac242faed7
matthieu <matthieu.cneude@gmail.com>
2020-03-30 17:56:14 +0200

    Lost my mind on some algorithm

--- clojure/brave-clojure/7-read-eval-macro/exercises.clj
+++ clojure/brave-clojure/7-read-eval-macro/exercises.clj
@@ -4,20 +4,26 @@
 '( 2 3)
 
 ; Create an infix function that takes a list like (1 + 3 * 4 - 5) and transforms it into the lists that Clojure needs in order to correctly evaluate the expression using operator precedence rules.
-(defmacro first-priority [infixed]
-  (loop [base infixed
-         new-list ()
+
+; TODO doesn't work for 3 * 2 * 5 (when * or / signs are one after the other)
+; TODO doesn't work for 3 * 2 + 5 either (2 twice)
+; TODO DOESNT WOOOORRKKK
+(defn first-priority [infixed]
+  (loop [new-list [] 
          list-index 0
          current-val (first infixed)]
-    (let [next-val (nth infixed (inc list-index))
-          prev-val (nth infixed (dec list-index))]
+      ; (println list-index)
+      (println new-list)
+      (println current-val)
       (cond  
-        (= list-index (count (- new-list 1))) (new-list)
-        (or (= current-val "*") ( current-val "/")) (recur base (conj new-list (list current-val prev-val next-val)) (inc list-index) (nth infixed (inc list-index))) 
-        :else (recur base (conj new-list current-val) (inc list-index) (nth infixed (inc list-index)))
-        ))))
+        (>= list-index (count infixed)) (seq new-list)
+        (or (= current-val '*) (= current-val '/)) (recur (into (vec (butlast new-list)) [(list current-val (nth new-list (dec list-index)) (nth infixed (inc list-index)))]) (+ list-index 1) (nth infixed (+ list-index 1)))
+        (and (list? (last new-list)) (number? current-val)) (recur new-list (+ list-index 1) (nth infixed (+ list-index 1) false))
+        :else (recur (into new-list [current-val]) (inc list-index) (nth infixed (inc list-index) list-index))
+        )))
 
-(first-priority (1 + 3 * 2 - 5))
+(first-priority '(1 + 3 * 2 * 5))
+(first-priority '(1 + 3 * 2 + 5))
 
 ; (defmacro infix
 ;   [infixed]
