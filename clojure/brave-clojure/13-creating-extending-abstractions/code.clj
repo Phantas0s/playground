586c01477000c63360e27428f57a86b77a910924
matthieu <matthieu.cneude@gmail.com>
2020-04-29 13:30:55 +0200

    Finish Clojure for Brave and True

--- clojure/brave-clojure/13-creating-extending-abstractions/code.clj
+++ clojure/brave-clojure/13-creating-extending-abstractions/code.clj
@@ -92,3 +92,60 @@
 ; => "meh about blorb"
 
 
+; Records
+
+
+; (ns were-records)
+(defrecord WereWolf [name title])
+
+; three way to create instance of WereWolf
+
+; same way we create Java object. List of fields need to have same order as defined.
+(WereWolf. "David" "London Tourist")
+; => #were_records.WereWolf{:name "David", :title "London Tourist"}
+
+(->WereWolf "Jacob" "Lead Shirt Discarder")
+; => #were_records.WereWolf{:name "Jacob", :title "Lead Shirt Discarder"}
+
+(map->WereWolf {:name "Lucian" :title "CEO of melodrama"})
+; => #were_records.WereWolf{:name "Lucian", :title "CEO of Melodrama"}
+
+; Import in another namespace
+; (ns monster-mash
+;   (:import [were_records WereWolf]))
+; (WereWolf. "David" "London Tourist")
+
+(def jacob (->WereWolf "Jacob" "Lead Shirt Discarder"))
+(.name jacob)
+; => "Jacob"
+(:name jacob)
+; => "Jacob"
+(get jacob :name)
+; => "Jacob"
+
+(= jacob (->WereWolf "Jacob" "Lead Shirt Discarder"))
+; => true
+(= jacob (WereWolf. "Jacob" "London Tourist"))
+; => false
+(= jacob (WereWolf. "David" "London Tourist"))
+; => false
+(= jacob {:name "Jacob" :title "Lead Shirt Discarder"})
+; => false
+
+(assoc jacob :title "Lead Third Wheel")
+; => #clojure_noob.core.WereWolf{:name "Jacob", :title "Lead Third Wheel"}
+
+(dissoc jacob :title)
+; => {:name "Jacob"} <- that's not a were_records.WereWolf
+
+; using protocol
+
+(defprotocol WereCreature
+  (full-moon-behavior [x]))
+
+(defrecord WereWolf [name title]
+  WereCreature
+  (full-moon-behavior [x]
+    (str name " will howl and murder")))
+
+(full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))
