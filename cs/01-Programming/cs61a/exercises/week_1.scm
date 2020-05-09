67be3a3a7b5894511bc2b1d58b43dd20bd2d755e
matthieu <matthieu.cneude@gmail.com>
2020-05-09 15:22:03 +0200

    Continue exercise berkeley sicp

--- cs/01-Programming/cs61a/exercises/week_1.scm
+++ cs/01-Programming/cs61a/exercises/week_1.scm
@@ -8,10 +8,44 @@
 ; | exercise 2 |
 ; +------------+
 
-(define (squares s)
-  (if (empty? s)
-             s
-             (squares (sentence (square (first s)) (bf(s))))))
+(define (squares nums)
+  (if (empty? nums)
+             '()
+             (se (square (first nums)) (squares (bf nums)))))
 
 
+; +------------+
+; | exercise 3 |
+; +------------+
 
+(define (switch s)
+  (if (empty? s) 
+    '()
+    (se (change-word (first s))
+        (switch (bf s)))))
+
+(define (change-word w)
+  (cond ((equal? w 'I) 'you)
+        ((equal? w 'i) 'you)
+        ((equal? w 'me) 'you)
+        ((equal? w 'you) 'me)
+        ((equal? w 'You) 'i)
+        (else w)))
+
+; +------------+
+; | exercise 4 |
+; +------------+
+
+(define (ordered? s)
+(cond ((equal? (length s) 1) true)
+      ((> (first s) (second s)) false)
+      (else (ordered? (bf s)))))
+
+; +------------+
+; | exercise 5 |
+; +------------+
+
+(define (ends-e s)
+  (cond ((empty? s) '())
+        ((equal? (last (first s)) 'e) (se (first s) (ends-e (bf s))))
+        (else (ends-e (bf s)))))
