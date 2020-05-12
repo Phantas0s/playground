a171c69e126c1d9f9544e950317f345d1dcf0b73
matthieu <matthieu.cneude@gmail.com>
2020-05-12 15:54:10 +0200

    More cs61a with SICP

--- cs/01-Programming/sicp/exercices/1.3.scm
+++ cs/01-Programming/sicp/exercices/1.3.scm
@@ -61,23 +61,121 @@
 (define (filter-accumulate predicate combiner null-value term a next b)
   (cond ((> a b) null-value)
         ((predicate a) (combiner (term a)
-                   (filter-accumulate predicate combiner null-value term (next a) next b)))
+                                 (filter-accumulate predicate combiner null-value term (next a) next b)))
         (else (combiner null-value 
                         (filter-accumulate predicate combiner null-value term (next a) next b)))))
 
-  (define (sum-squares-prime a b) 
-    (filter-accumulate prime? + 0 identity a inc b))
+(define (sum-squares-prime a b) 
+  (filter-accumulate prime? + 0 identity a inc b))
 
-  (define (product-relative-prime b) 
-    (filter-accumulate (lambda (a) (equal? (gcd a b) 1)) * 1 identity 0 inc b))
+(define (product-relative-prime b) 
+  (filter-accumulate (lambda (a) (equal? (gcd a b) 1)) * 1 identity 0 inc b))
 
 **PERFECT**
 
-  ; +----------------+
-  ; |  EXERCISE 1.34 |
-  ; +----------------+
+; +----------------+
+; |  EXERCISE 1.34 |
+; +----------------+
 
-  ; (define (f g) (g 2))
-  ; What happens if we do (f f)?
+; (define (f g) (g 2))
+; What happens if we do (f f)?
 
-  ; (f f) would expand to (f 2) which would expand to (2 2). At that point, the interpreter will throw an error
+; (f f) would expand to (f 2) which would expand to (2 2). At that point, the interpreter will throw an error
+
+; +---------------+
+; | Exercise 1.40 |
+; +---------------+
+
+; (define dx 0.00001)
+; (define (close-enough? x y) 
+;   (< (abs (- x y)) 0.001))
+
+; (define (fixed-point f first-guess)
+;   (define (close-enough? v1 v2)
+;     (< (abs (- v1 v2)) 
+;        tolerance))
+;   (define (try guess)
+;     (let ((next (f guess)))
+;       (if (close-enough? guess next)
+;           next
+;           (try next))))
+;   (try first-guess))
+
+; (define (deriv g)
+;   (lambda (x)
+;     (/ (- (g (+ x dx)) (g x))
+;        dx)))
+
+
+; (define (newton-transform g)
+;   (lambda (x)
+;     (- x (/ (g x) 
+;             ((deriv g) x)))))
+
+; (define (newtons-method g guess)
+;   (fixed-point (newton-transform g) 
+;                guess))
+
+(define (cube x) (* x x x))
+(define (cubic a b c) (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+**PERFECT**
+
+; +---------------+
+; | Exercise 1.41 |
+; +---------------+
+
+(define (double p) (lambda (x) (p (p x))))
+
+(((double (double double)) inc) 5)
+
+; Going inward to outward: (double double) will double a double, so 2^2 execution of the argument procedure.
+; Executing double on it will do 2^(2*2) execution 2^4 execution of the argument procedure.
+; Therefore, inc will be executed 16 times. The result is 5 + 16 = 21
+; It means that something like (((double (double (double double))) inc) 5) will execute inc 2^(2*2*2) = 256 times!
+
+; +---------------+
+; | Exercise 1.43 |
+; +---------------+
+
+; ((repeated square 2) 5) 
+; => 625
+
+(define (repeated p n)
+  (define (sub p n x)
+    (if (= n 0)
+      x
+      (p (sub p (- n 1) x))))
+  (lambda (x) (sub p n x)))
+
+**GOOD**
+
+; +---------------+
+; | Exercise 1.46 |
+; +---------------+
+
+(define (iterative-improve good-enough? improve)
+  (define (sub-improve guess)
+    (if (good-enough? guess)
+      guess
+      (sub-improve (improve guess))))
+  (lambda (guess) (sub-improve guess)))
+
+; (define (improve guess x)
+;   (average guess (/ x guess)))
+
+; (define (good-enough? guess x)
+;   (< (abs (- (square guess) x)) 0.001))
+
+; (define (sqrt-iter guess x)
+;   (if (good-enough? guess x)
+;     guess
+;     (sqrt-iter (improve guess x) x))) 
+
+; (define (sqrt x)
+;   (sqrt-iter 1.0 x))
+
+(define (sqrt x)
+    ((iterative-improve 
+     good-enough? 
+     (lambda (x) (improve x 1.0))) x)
