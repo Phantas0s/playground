f24834dc353491c4df8c9117f1d0a68e228c5d62
matthieu <matthieu.cneude@gmail.com>
2020-05-21 14:01:06 +0200

    Continue SICP exercises

--- cs/01-Programming/cs61a/exercises/week_3.scm
+++ cs/01-Programming/cs61a/exercises/week_3.scm
@@ -30,9 +30,20 @@
 ;  ; as in the original version
 
 (define (count-change amount) (cc amount 5))
-(define ((= amount 0) 1)
-  (cond (cc amount kinds-of-coins)
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination
+                          kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (new-count-change amount) (cc amount 5))
+(define (new-cc amount kinds-of-coins)
+  (cond ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        ((= amount 0) 1)
         (else (+ (cc amount
                      (- kinds-of-coins 1))
                  (cc (- amount
@@ -46,4 +57,27 @@
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
 
-If we change the two first condition branches, 
+
+; The value would be different with (cc 0 0) - for the case in the book, it returns 1, and for the new-cc, it returns 0.
+
+; +------------+
+; | Exercise 4 |
+; +------------+
+(define (expt b n)
+  (if (= n 0)
+    1
+    (* b (expt b (- n 1)))))
+
+(define (expt-i b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+    product
+    (expt-iter b
+               (- counter 1)
+               (* b product))))
+
+; Give An Algebraic Formula Relating The Values Of The Parameters B, N, Counter, And
+; Product Of The Expt And Exp-iter Procedures Given Near The Top Of Page 45 Of Abelson
+; And Sussman. (The Kind Of Answer We’Re Looking For Is “The Sum Of B, N, And Counter Times
+; Product Is Always Equal To 37.”)
