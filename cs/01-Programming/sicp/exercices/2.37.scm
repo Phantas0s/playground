d3b5c064648dcbe61dd669f165a4453d3998d876
matthieu <matthieu.cneude@gmail.com>
2020-06-25 14:56:30 +0200

    MORE EXERCISEE

--- cs/01-Programming/sicp/exercices/2.37.scm
+++ cs/01-Programming/sicp/exercices/2.37.scm
@@ -1,24 +1,36 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
   (map
-    (lambda (x) (dot-product x v)) m))
+    (lambda (x) (dot-product v x)) m))
 
 (define (transpose mat)
   (accumulate-n
-    ⟨??⟩ ⟨??⟩ mat))
+    cons '() mat))
 
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
     (map
-      (lambda (x) (matrix-*-vector n)) m)))
-
+      (lambda (x) (matrix-*-vector cols x)) m)))
 
 (dot-product (list 1 2 3) (list 7 9 11))
 ; => 58
 
-(matrix-*-vector (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list 2 1 3))
+(define mat-1 (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
+
+(matrix-*-vector mat-1 (list 2 1 3))
 ; => '(13 31 49)
 
-; (matrix-*-matrix (list (list 1 2 3) (list 4 5 6)) (list (list 7 8) (list 9 10) (list 11 12)))
+(transpose mat-1)
+; => '((1 4 7) (2 5 8) (3 6 9))
+
+(matrix-*-matrix mat-1 mat-1)
+
+**PERFECT**
