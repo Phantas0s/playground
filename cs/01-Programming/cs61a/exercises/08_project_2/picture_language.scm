7eb9559e70d56107faa7637158fd24e54eefd518
matthieu <matthieu.cneude@gmail.com>
2020-07-01 15:52:00 +0200

    Begin the project picture language

--- cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
+++ cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
@@ -1,2 +1,43 @@
 ; 2.2.4 Example: A Picture Language (p 172 of SICP)
 
+(define wave2 (beside wave (flip-vert wave)))
+(define wave4 (below wave2 wave2))
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4 (flipped-pairs wave))
+
+(define (right-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (right-split painter (- n 1))))
+      (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+    painter
+    (let ((up (up-split painter (- n 1)))
+          (right (right-split painter (- n 1))))
+      (let ((top-left (beside up up))
+            (bottom-right (below right right))
+            (corner (corner-split painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
++---------------+
+| exercise 2.24 |
++---------------+
+
+(define (up-split painter n)
+(if (= n 0)
+    painter
+    (let ((smaller (up-split painter (- n 1))))
+      (below (beside smaller smaller) painter))))
+
