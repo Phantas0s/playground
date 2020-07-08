a079d20352d7dd232a9c997c50eefb609c82cfe2
matthieu <matthieu.cneude@gmail.com>
2020-07-08 14:54:49 +0200

    Continue exercises of the picture language

--- cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
+++ cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
@@ -255,16 +255,136 @@
 (define (outline-painter frame)
   ((segments->painter (outline-segments frame))) frame)
 
- ; (define (outline->painter frame) 
- ;   (let ((origin2 (make-vect  
- ;                   (- (xcor-vect (edge2-frame frame))  
- ;                      (xcor-vect (origin-frame frame))) 
- ;                   (- (ycor-vect (edge1-frame frame))  
- ;                      (ycor-vect (origin-frame frame)))))) 
- ;     (segments->painter  
- ;      (list
- ;       (make-segment (origin-frame frame) (edge1-frame frame)) 
- ;       (make-segment (edge1-frame frame) origin2) 
- ;       (make-segment origin2 (edge2-frame frame)) 
- ;       (make-segment (edge2-frame frame) (origin-frame frame)))))) 
+;(define (outline->painter frame) 
+;   (let ((origin2 (make-vect  
+;                   (- (xcor-vect (edge2-frame frame))  
+;                      (xcor-vect (origin-frame frame))) 
+;                   (- (ycor-vect (edge1-frame frame))  
+;                      (ycor-vect (origin-frame frame)))))) 
+;     (segments->painter  
+;      (list           
+;       (make-segment (origin-frame frame) (edge1-frame frame)) 
+;       (make-segment (edge1-frame frame) origin2) 
+;       (make-segment origin2 (edge2-frame frame)) 
+;       (make-segment (edge2-frame frame) (origin-frame frame)))))) 
   
+; (define (X->painter frame) 
+;   (let ((origin2 (make-vect  
+;                   (- (xcor-vect (edge2-frame frame))  
+;                      (xcor-vect (origin-frame frame))) 
+;                   (- (ycor-vect (edge1-frame frame))  
+;                      (ycor-vect (origin-frame frame)))))) 
+;     (segments->painter  
+;      (list           
+;       (make-segment (origin-frame frame) origin2) 
+;       (make-segment (edge1-frame frame) (edge2-frame frame)))))) 
+  
+; (define (diamond->painter frame) 
+;   (let ((midpoint1 (sub-vect (edge1-frame frame) (origin-frame frame)))  
+;         (midpoint2 (sub-vect origin2 (edge1-frame frame)))  
+;         (midpoint3 (sub-vect origin2 (edge2-frame frame))) 
+;         (midpoint4 (sub-vect (edge2-frame frame) (origin-frame frame)))) 
+;     (segments->painter  
+;      (list           
+;       (make-segment midpoint1 midpoint2) 
+;       (make-segment midpoint2 midpoint3) 
+;       (make-segment midpoint3 midpoint4) 
+;       (make-segment midpoint4 midpoint1))))) 
+  
+; (define wave 
+;   (segments->painter (list 
+;                       (make-segment (make-vect .25 0) (make-vect .35 .5)) 
+;                       (make-segment (make-vect .35 .5) (make-vect .3 .6)) 
+;                       (make-segment (make-vect .3 .6) (make-vect .15 .4)) 
+;                       (make-segment (make-vect .15 .4) (make-vect 0 .65)) 
+;                       (make-segment (make-vect 0 .65) (make-vect 0 .85)) 
+;                       (make-segment (make-vect 0 .85) (make-vect .15 .6)) 
+;                       (make-segment (make-vect .15 .6) (make-vect .3 .65)) 
+;                       (make-segment (make-vect .3 .65) (make-vect .4 .65)) 
+;                       (make-segment (make-vect .4 .65) (make-vect .35 .85)) 
+;                       (make-segment (make-vect .35 .85) (make-vect .4 1)) 
+;                       (make-segment (make-vect .4 1) (make-vect .6 1)) 
+;                       (make-segment (make-vect .6 1) (make-vect .65 .85)) 
+;                       (make-segment (make-vect .65 .85) (make-vect .6 .65)) 
+;                       (make-segment (make-vect .6 .65) (make-vect .75 .65)) 
+;                       (make-segment (make-vect .75 .65) (make-vect 1 .35)) 
+;                       (make-segment (make-vect 1 .35) (make-vect 1 .15)) 
+;                       (make-segment (make-vect 1 .15) (make-vect .6 .45)) 
+;                       (make-segment (make-vect .6 .45) (make-vect .75 0)) 
+;                       (make-segment (make-vect .75 0) (make-vect .6 0)) 
+;                       (make-segment (make-vect .6 0) (make-vect .5 .3)) 
+;                       (make-segment (make-vect .5 .3) (make-vect .4 0)) 
+;                       (make-segment (make-vect .4 0) (make-vect .25 0)) 
+;                       ))) 
+; ;George! 
+  
+; I don't how to solve the exercise or the solutions found on Internet. THe one below looks the most approapriate (it seems to me that we need to define the painter in term of a frame), but I don't understand why you should substract the points from origin for origin2.
+
+; I need to go back to some algebra I guess. It's on my study map.
+
+; **PASS** **WRONG**
+
+; -----------------
+
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                   new-origin
+                   (sub-vect (m corner1) new-origin)
+                   (sub-vect (m corner2) new-origin)))))))
+
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 1.0 1.0) ; new end of edge1
+                     (make-vect 0.0 0.0))) ; new end of edge2
+
+(define (shrink-to-upper-right painter)
+  (transform-painter
+    painter (make-vect 0.5 0.5)
+    (make-vect 1.0 0.5) (make-vect 0.5 1.0)))
+
+
+(define (shrink-to-upper-right painter)
+  (transform-painter
+    painter (make-vect 0.5 0.5)
+    (make-vect 1.0 0.5) (make-vect 0.5 1.0)))
+
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+
+(define (squash-inwards painter)
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
+
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+            (transform-painter
+              painter1
+              (make-vect 0.0 0.0)
+              split-point
+              (make-vect 0.0 1.0)))
+          (paint-right
+            (transform-painter
+              painter2
+              split-point
+              (make-vect 1.0 0.0)
+              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+
