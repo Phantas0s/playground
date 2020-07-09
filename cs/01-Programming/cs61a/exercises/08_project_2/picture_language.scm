c82bfebfb891b5fa808a8007e19fd5aaac6e5424
matthieu <matthieu.cneude@gmail.com>
2020-07-09 15:48:41 +0200

    Do some square unit transformations

--- cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
+++ cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
@@ -326,6 +326,16 @@
 
 ; -----------------
 
+; Unit square and edges
+
+;      0.1        1.1
+;       +----------+
+;       |          |
+;       | e2       |
+;       |          |
+;       |   e1     |
+;       +----------+
+;      0.0        1.0
 
 (define (transform-painter painter origin corner1 corner2)
   (lambda (frame)
@@ -387,4 +397,77 @@
         (paint-left frame)
         (paint-right frame)))))
 
+; +---------------+
+; | exercise 2.50 |
+; +---------------+
 
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+**PERFECT**
+
+(define (rotatecw-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0) 
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+**PERFECT**
+
+
+(define (rotatecw-180 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+**PERFECT**
+
+; +---------------+
+; | exercise 2.51 |
+; +---------------+
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+            (transform-painter
+              painter1
+              (make-vect 0.0 0.0)
+              split-point
+              (make-vect 0.0 1.0)))
+          (paint-right
+            (transform-painter
+              painter2
+              split-point
+              (make-vect 1.0 0.0)
+              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-down
+            (transform-painter
+              painter1
+              (make-vect 0.0 0.0)
+              (make-vect 1.0 0.0)
+              split-point))
+          (paint-up
+            (transform-painter
+              painter2
+              split-point
+              (make-vect 1.0 0.5)
+              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-down frame)
+        (paint-up frame)))))
+
+**PERFECT**
+
+(define (below painter1 painter2)
+  )
