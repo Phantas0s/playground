e1a569608ebea1534d25649a239912f78d83fa03
matthieu <matthieu.cneude@gmail.com>
2020-07-12 13:30:52 +0200

    Finish the picture language project

--- cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
+++ cs/01-Programming/cs61a/exercises/08_project_2/picture_language.scm
@@ -469,5 +469,50 @@
 
 **PERFECT**
 
-(define (below painter1 painter2)
-  )
+; +---------------+
+; | exercise 2.52 |
+; +---------------+
+
+a. 
+
+(define wave 
+  (segments->painter (list 
+                       (make-segment (make-vect .25 0) (make-vect .35 .5)) 
+                       (make-segment (make-vect .35 .5) (make-vect .3 .6)) 
+                       (make-segment (make-vect .3 .6) (make-vect .15 .4)) 
+                       (make-segment (make-vect .15 .4) (make-vect 0 .65)) 
+                       (make-segment (make-vect 0 .65) (make-vect 0 .85)) 
+                       (make-segment (make-vect 0 .85) (make-vect .15 .6)) 
+                       (make-segment (make-vect .15 .6) (make-vect .3 .65)) 
+                       (make-segment (make-vect .3 .65) (make-vect .4 .65)) 
+                       (make-segment (make-vect .4 .65) (make-vect .35 .85)) 
+                       (make-segment (make-vect .35 .85) (make-vect .4 1)) 
+                       (make-segment (make-vect .4 1) (make-vect .6 1)) 
+                       (make-segment (make-vect .6 1) (make-vect .65 .85)) 
+                       (make-segment (make-vect .65 .85) (make-vect .6 .65)) 
+                       (make-segment (make-vect .6 .65) (make-vect .75 .65)) 
+                       (make-segment (make-vect .75 .65) (make-vect 1 .35)) 
+                       (make-segment (make-vect 1 .35) (make-vect 1 .15)) 
+                       (make-segment (make-vect 1 .15) (make-vect .6 .45)) 
+                       (make-segment (make-vect .6 .45) (make-vect .75 0)) 
+                       (make-segment (make-vect .75 0) (make-vect .6 0)) 
+                       (make-segment (make-vect .6 0) (make-vect .5 .3)) 
+                       (make-segment (make-vect .5 .3) (make-vect .4 0)) 
+                       (make-segment (make-vect .4 0) (make-vect .25 0)) 
+                       (make-segment (make-vect .65 .8) (make-vect .75 .8))
+                       ))) 
+
+; b 
+(define (corner-split painter n) 
+  (if (= n 0) 
+    painter 
+    (beside (below painter (up-split painter (- n 1))) 
+            (below (right-split painter (- n 1)) (corner-split painter (- n 1)))))) 
+
+;; c 
+(define (square-limit painter n) 
+  (let ((combine4 (square-of-four flip-vert rotate180 
+                                  identity flip-horiz))) 
+    (combine4 (corner-split painter n)))) 
+
+**GOOD**
