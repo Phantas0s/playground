368fd54b1399a7df80fabab95283cc08b983e1db
matthieu <matthieu.cneude@gmail.com>
2020-07-23 11:45:39 +0200

    Just begin next exercise

---
+++ cs/01-Programming/sicp/exercices/2.75.scm
@@ -0,0 +1,6 @@
+; Implement the constructor make-from-mag-
+; ang in message-passing style. This procedure should be anal-
+; ogous to the make-from-real-imag procedure given above.
+
+(define (make-from-mag-ang)
+  )
