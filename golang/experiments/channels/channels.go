63e5559478689c526276db5ba476db21db5d0e69
matthieu <matthieu.cneude@gmail.com>
2021-01-20 15:38:00 +0100

    Fooling around with channels

---
+++ golang/experiments/channels/channels.go
@@ -0,0 +1,74 @@
+// Little demo to show the difference between buffered and unbuffered channel
+
+// In short: any read block, for buffered or unbuffered channels.
+// Writing doesn't block for buffered channel iff the channel has some space
+// Writing always block for unbuffured channel if the channel is nowhere read (on another goroutine)
+
+// Is make(chan bool, 0) and make(chan bool) the same?
+
+// It means that the only difference between make(chan bool) and make(chan bool, 1) is at the write level: the first one will block, the second won't.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	bufferedChannel()
+	unbuffuredChannel()
+	bufferedChannelWrite()
+	// unbufferedChannelWrite()
+	biggerBufferedChannel()
+}
+
+func bufferedChannel() {
+	c := make(chan bool, 1)
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("In goroutine with buffured channel (size 1)")
+		c <- true
+	}()
+	<-c // block till receive a value
+	fmt.Println("Buffered channel finished (size 1)")
+}
+
+func biggerBufferedChannel() {
+	c := make(chan bool, 3)
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("In goroutine with buffured channel (size 3)")
+		c <- true
+	}()
+	<-c // block till receive a value
+	fmt.Println("Buffered channel finished (size 3)")
+}
+
+func unbuffuredChannel() {
+	c := make(chan bool)
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("In goroutine with unbuffered channel")
+		c <- true
+	}()
+	// will block if there is not the next line (no receiver)
+	<-c
+	fmt.Println("unbuffered channel finished")
+}
+
+func bufferedChannelWrite() {
+	c := make(chan bool, 1)
+	// Doesn't block
+	c <- true
+	// If next line uncommented, will block - deadlock (channel full already)
+	// c <- true
+	fmt.Println("Finished to write to buffered channel (size 1)")
+}
+
+func unbufferedChannelWrite() {
+	c := make(chan bool)
+	// will block here - deadlock
+	c <- true
+	fmt.Println("Finished to write to unbuffered channel")
+}
